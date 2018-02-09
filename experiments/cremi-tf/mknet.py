71ebd558fb54f445c5c25a170d42aa30b1272a12
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2018-02-09 14:30:38 -0500

    add construction of inference graph (reduced, larger input)

--- experiments/cremi-tf/mknet.py
+++ experiments/cremi-tf/mknet.py
@@ -3,6 +3,34 @@ import networks
 import tensorflow as tf
 import json
 
+def cremi_unet_inference(name='unet_inference', sample_to_isotropy=False):
+    in_shape = (88, 808, 808)
+    n_channels = 12
+
+    # These values reproduce jans network
+    initial_fmaps = 12
+    fmap_increase = 5
+    downsample_factors = [[1, 3, 3], [1, 3, 3], [3, 3, 3]] if sample_to_isotropy else \
+        [[1, 3, 3], [1, 3, 3], [1, 3, 3]]
+
+    raw = tf.placeholder(tf.float32, shape=in_shape)
+    raw_batched = tf.reshape(raw, (1, 1,) + in_shape)
+
+    unet = networks.unet(raw_batched, initial_fmaps, fmap_increase, downsample_factors)
+
+    affs_batched = networks.conv_pass(unet,
+                                      kernel_size=1,
+                                      num_fmaps=n_channels,
+                                      num_repetitions=1,
+                                      activation='sigmoid')
+
+    output_shape_batched = affs_batched.get_shape().as_list()
+    output_shape = output_shape_batched[1:]  # strip the batch dimension
+
+    affs = tf.reshape(affs_batched, output_shape)
+
+    tf.train.export_meta_graph(filename='%s.meta' % name)
+
 
 def cremi_unet(name='unet', sample_to_isotropy=False):
     in_shape = (84, 268, 268)
@@ -67,3 +95,4 @@ def cremi_unet(name='unet', sample_to_isotropy=False):
 if __name__ == "__main__":
     sample_to_isotropy = True
     cremi_unet(sample_to_isotropy=sample_to_isotropy)
+    cremi_unet_inference(sample_to_isotropy=sample_to_isotropy)
