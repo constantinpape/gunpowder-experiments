88d0c11ca175f023057507cdb846e52ca409b392
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-04-19 19:04:02 -0400

    Start preparation for multiscale fullscale

---
+++ backend/prepare_multiscale_groundtruth.py
@@ -0,0 +1,11 @@
+import affinities
+
+
+def prepare(sample, block_sizes):
+    pass
+
+
+if __name__ == '__main__':
+    sample = 'A'
+    block_sizes = [[1, 10, 10], [2, 20, 20], [4, 40, 40]]
+    prepare(sample, block_sizes)
